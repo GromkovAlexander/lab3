f4a9d527d84aef05e5cfa979c115eaa3a632f03b
GromkovAlexander <gromkovalex@mail.ru>
2019-11-01 15:13:22 +0300

    Scripted auto-commit on change (2019-11-01 15:13:22) by gitwatch.sh

--- src/main/java/bmstu/lab3/AirportsInfo.java
+++ src/main/java/bmstu/lab3/AirportsInfo.java
@@ -8,6 +8,8 @@ import scala.Tuple2;
 public class AirportsInfo {
 
     private final static String COMMA = ",";
+    private final static String QUOTES = "\"";
+    private final static String EMPTY = "";
     private static final int COLUMN_AIRPORT_CODE = 0;
     private static final int COLUMN_AIRPORT_DESCRIPTION = 1;
 
@@ -17,9 +19,13 @@ public class AirportsInfo {
         return airports.filter(s -> !s.equals(title));
     }
 
+    public static String deleteQuotes(String s) {
+        return s.replaceAll(QUOTES, EMPTY);
+    }
+
     public static String getValue(String s, int pos) {
         String[] columns = s.split(COMMA, 2);
-        return columns[pos];
+        return deleteQuotes(columns[pos]);
     }
 
     public static JavaPairRDD<Integer, String> sortKV(JavaRDD<String> file) {
