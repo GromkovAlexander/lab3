07231f5387ace9786ed541d8c1fac0814cd03a3b
GromkovAlexander <gromkovalex@mail.ru>
2019-11-01 13:39:31 +0300

    Scripted auto-commit on change (2019-11-01 13:39:31) by gitwatch.sh

--- src/main/java/bmstu/lab3/Main.java
+++ src/main/java/bmstu/lab3/Main.java
@@ -4,12 +4,15 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> airports = sc.textFile(args[0]);
+        airports.flatMap(x -> Arrays.stream(x.split(",")).iterator());
         airports.saveAsTextFile("output1");
 
 
