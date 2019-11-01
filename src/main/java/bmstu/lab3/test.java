ddf58d5b69f7895a17a4696ccf8f7dc61d71adbb
GromkovAlexander <gromkovalex@mail.ru>
2019-11-01 21:25:09 +0300

    Scripted auto-commit on change (2019-11-01 21:25:09) by gitwatch.sh

--- src/main/java/bmstu/lab3/test.java
+++ src/main/java/bmstu/lab3/test.java
@@ -5,7 +5,7 @@ public class test {
         float[] flightsInfo = new float[3];
         flightsInfo[0] = 1;
         flightsInfo[1] = 4;
-        flightsInfo[2] = 31;
+//        flightsInfo[2] = 31;
 
         for (int i = 0; i < flightsInfo.length; i++) {
             System.out.println(flightsInfo[i]);
