76edb39faa78f6708b24eb3eba0f2d8f0db71a4a
GromkovAlexander <gromkovalex@mail.ru>
2019-11-01 14:20:22 +0300

    Scripted auto-commit on change (2019-11-01 14:20:22) by gitwatch.sh

--- src/main/java/bmstu/lab3/AirportsInfo.java
+++ src/main/java/bmstu/lab3/AirportsInfo.java
@@ -2,8 +2,11 @@ package bmstu.lab3;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class AirportsInfo {
+
+    private final static String COMMA = ",";
 
     public static JavaRDD<String> loadData(JavaSparkContext sc, String path) {
         JavaRDD<String> airports = sc.textFile(path);
@@ -11,9 +14,17 @@ public class AirportsInfo {
         return airports.filter(line -> !line.equals(title));
     }
 
+    public static String getValue(String s, int pos) {
+        String[] columns = s.split(COMMA, 2);
+        return columns[pos];
+    }
+
     public static JavaRDD<String> sortKV(JavaRDD<String> file) {
         JavaRDD<String> kv = file.mapToPair(
-                
+                s -> new Tuple2<>(
+                        Integer.parseInt(getValue(s, 0)),
+                        getValue(s, 1)
+                )
         );
 
     }
--- src/main/java/bmstu/lab3/Main.java
+++ src/main/java/bmstu/lab3/Main.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 public class Main {
 
-    private final static String COMMA = ",";
+
 
 
 
