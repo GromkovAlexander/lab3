35ec208460e4ffbbb46b482e20d53054b4b98c26
GromkovAlexander <gromkovalex@mail.ru>
2019-11-01 13:25:04 +0300

    Scripted auto-commit on change (2019-11-01 13:25:04) by gitwatch.sh

--- src/main/java/bmstu/lab3/Main.java
+++ src/main/java/bmstu/lab3/Main.java
@@ -10,7 +10,6 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> airports = sc.textFile(args[0]);
-
         airports.saveAsTextFile("output1");
 
 
