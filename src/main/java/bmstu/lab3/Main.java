78e4ee4571727b4ba04b5f1d8ab693d79d4869e0
GromkovAlexander <gromkovalex@mail.ru>
2019-11-01 13:09:43 +0300

    Scripted auto-commit on change (2019-11-01 13:09:43) by gitwatch.sh

--- src/main/java/bmstu/lab3/Main.java
+++ src/main/java/bmstu/lab3/Main.java
@@ -1,7 +1,23 @@
 package bmstu.lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class Main {
     public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airports = sc.textFile(args[0]);
+
+        airports.saveAsTextFile("output1.txt");
+
         
+
+        JavaRDD<String> delays = sc.textFile(args[1]);
+        delays.saveAsTextFile("output2.txt");
+
+
     }
 }
